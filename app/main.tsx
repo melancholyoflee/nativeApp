48c1f5ad30dc7806f30b71512dcb957d05af12dd
melancholyoflee <saiyji6saiyiji6@yahoo.com.tw>
2024-12-07 07:41:44 +0800

    add camera

--- app/main.tsx
+++ app/main.tsx
@@ -1,6 +1,35 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {Text, Button, Image, View, StyleSheet ,} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { BottomNavigation } from 'react-native-paper';
+import CameraPage from './Camera';
+
+function ImagePickerExample() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images', 'videos'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+    </View>
+  );
+}
 
 const ParkingMap = () => (
   <View style={styles.page}>
@@ -9,9 +38,7 @@ const ParkingMap = () => (
 );
 
 const Camera = () => (
-  <View style={styles.page}>
-    <Text style={styles.text}>拍照</Text>
-  </View>
+  ImagePickerExample()
 );
 
 const Attractions = () => (
@@ -30,7 +57,7 @@ export default function MainScreen() {
 
   const renderScene = BottomNavigation.SceneMap({
     parkingMap: ParkingMap,
-    camera: Camera,
+    camera: CameraPage,
     attractions: Attractions,
   });
 
@@ -44,6 +71,15 @@ export default function MainScreen() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
   page: {
     flex: 1,
     justifyContent: 'center',
